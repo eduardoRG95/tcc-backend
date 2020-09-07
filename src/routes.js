e58e724e26931d7a11125f20382aae91736d7c02
eduardoRG95 <eduardoraupp7@gmail.com>
2020-09-07 13:52:06 -0300

    teste

--- src/routes.js
+++ src/routes.js
@@ -4,20 +4,24 @@ const connection = require('./database/connection')
 
 const routes = express.Router();
 
+routes.get('/admin', async (request, response) => { 
+    const admin = await connection('UserAdmin').select('*');
+    return response.json(admin)
+});
 
-routes.post('/admin', (request, response) => {
+routes.post('/admin', async (request, response) => {
     const { nome, email, senha} = request.body;
 
     const id = crypto.randomBytes(5).toString('HEX')
 
-    connection('UserAdmin').insert({
+    await connection('UserAdmin').insert({
         id,
         nome,
         email,
         senha
     })
 
-    return response.json({ });
+    return response.json({ id });
 });
 
 module.exports = routes;
